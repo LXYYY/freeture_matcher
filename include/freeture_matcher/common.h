a0db64c9ebd0a3703337e0d9b29fc80acc87b717
Mike Xiangyu Liu <xiangyu002@e.ntu.edu.sg>
2021-02-03 11:08:03 +0800

    changed open3d version and cmakelists. WIP add rviz keypoints visualization and minsubmap

--- include/freeture_matcher/common.h
+++ include/freeture_matcher/common.h
@@ -2,6 +2,7 @@
 #define INCLUDE_FREETURE_MATCHER_COMMON_H_
 
 #include <Open3D/Geometry/PointCloud.h>
+#include <Open3D/Geometry/TriangleMesh.h>
 #include <Open3D/Registration/Feature.h>
 #include <Open3D/Registration/Registration.h>
 #include <voxblox/core/common.h>
@@ -62,6 +63,21 @@ typedef open3d::registration::Feature O3dFeature;
 using Feature = Eigen::VectorXd;
 using FeatureMatrix = AlignedVector<Eigen::VectorXd>;
 using RegistrationResult = open3d::registration::RegistrationResult;
+
+struct MinSubmap {
+  MinSubmap() = default;
+  MinSubmap(const PointcloudV& _keypoints, O3dFeature _features,
+            const Transformation& _T_G_S,
+            const open3d::geometry::TriangleMesh& _mesh)
+      : keypoints(_keypoints),
+        features(_features),
+        T_G_S(_T_G_S),
+        mesh(_mesh) {}
+  PointcloudV keypoints;
+  O3dFeature features;
+  Transformation T_G_S;
+  open3d::geometry::TriangleMesh mesh;
+};
 }  // namespace voxblox
 
 #endif  // INCLUDE_FREETURE_MATCHER_COMMON_H_
